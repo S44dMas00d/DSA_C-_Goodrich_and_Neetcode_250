6d9269dbdbd551df75028e08e1e79512a27ce0e2
S44DMA500D <saadmasood192@gmail.com>
2026-07-20 01:17:05 +0500

    working on 2206 right now and the algo is becoming more complex than the problem is worth - tabling and moving

--- ch03/2206_Divide_Array_Into_Equal_Pairs.cpp
+++ ch03/2206_Divide_Array_Into_Equal_Pairs.cpp
@@ -12,11 +12,18 @@ public:
         if (num_size % 2 != 0 && num_size <= 1) {
             return false;
         }
+        auto findInVector = [](const std::vector<int>& v, int value) {
+            for (auto iter = v.begin(); iter != v.end(); ++iter) {
+                if (*iter == value)
+                    return true;
+            }
+            return false;
+        };
         int pair_count = 0;
         int i = 0;
         int j = 1;
-        int last_pair_i = NULL;
-        int last_pair_j = NULL;
+        vector<int> last_pair_i_s;
+        vector<int> last_pair_j_s;
         bool pair_checked = false;
         while ((i < j) && j < num_size) {
             if (nums[i] == nums[j]) {
@@ -24,17 +31,17 @@ public:
                 pair_count++;
             }
             if (pair_checked) {
-                if (i + 1 == last_pair_j) {
-                    last_pair_i = i;
-                    i = last_pair_j + 1;
+                if (findInVector(last_pair_j_s, i + 1)) {
+                    last_pair_i_s.push_back(i);
+                    i = last_pair_j_s.back() + 1;
                     if (i == j) {
                         i++;
                     }
                 } else {
-                    last_pair_i = i;
+                    last_pair_i_s.push_back(i);
                     i++;
                 }
-                last_pair_j = j;
+                last_pair_j_s.push_back(j);
                 j = i + 1;
             } else {
                 pair_checked = false;
